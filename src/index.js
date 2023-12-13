510a016a19eb49571fca47bd4ab5ec9d61861928
Joseph Lee <josephlee3004@gmail.com>
2023-12-13 12:01:18 -0500

    Load fonts and images using webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("INIT");
+import "./styles.css";
+import twoScoopConeImg from "./assets/two-scoop-cone.png";
+
+const content = document.querySelector("#content");
+
+// Add two scoop cone image to content
+const twoScoopCone = new Image();
+twoScoopCone.src = twoScoopConeImg;
+twoScoopCone.setAttribute("id", "two-scoop-cone");
+twoScoopCone.setAttribute("alt", "Two scoop ice cream cone image");
+
+content.insertBefore(twoScoopCone, content.lastElementChild);
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,28 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/[hash][ext][query]",
+                },
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/[hash][ext][query]",
+                },
+            },
+        ],
+    },
     plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
     devServer: {
         static: "./src",
