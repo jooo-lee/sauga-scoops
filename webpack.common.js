ba525857f971f7aa243d59ee16cc45044d856913
Joseph Lee <josephlee3004@gmail.com>
2023-12-13 12:35:53 -0500

    Use webpack-merge

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,7 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
     entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
     module: {
         rules: [
             {
@@ -31,8 +24,9 @@ module.exports = {
             },
         ],
     },
-    plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
-    devServer: {
-        static: "./src",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
 };
---
+++ webpack.dev.js
@@ -0,0 +1,12 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+    mode: "development",
+    devtool: "inline-source-map",
+    plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+    devServer: {
+        static: "./dist",
+    },
+});
