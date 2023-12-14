13e30c2427386adc55181f52b51a5fd130952aac
Joseph Lee <josephlee3004@gmail.com>
2023-12-14 13:58:09 -0500

    Initialize homepage content div using JavaScript

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import "./styles.css";
-import twoScoopConeImg from "./assets/two-scoop-cone.png";
+import initPage from "./initPage.js";
 import GitHubIconSvg from "./assets/github-mark-pink.svg";
 
-const content = document.querySelector("#content");
-
-// Add two scoop cone image to content
-const twoScoopCone = new Image();
-twoScoopCone.src = twoScoopConeImg;
-twoScoopCone.setAttribute("id", "two-scoop-cone");
-twoScoopCone.setAttribute("alt", "Two scoop ice cream cone image");
-content.insertBefore(twoScoopCone, content.lastElementChild);
+initPage();
 
 // Add GitHub logo as link in footer
 const GitHubIcon = new Image();
