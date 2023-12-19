2b74f7264cde4f850bb56d1b06680cfd89d49233
Joseph Lee <josephlee3004@gmail.com>
2023-12-19 15:21:11 -0500

    Create and style contact content

--- dist/main.js
+++ dist/main.js
@@ -264,8 +264,57 @@ function createMenuContent(parent) {
 /* harmony default export */ const menu = (createMenuContent);
 
 ;// CONCATENATED MODULE: ./src/js/contact.js
+const contact_location = (() => {
+    const div = document.createElement("div");
+
+    const h2 = document.createElement("h2");
+    h2.textContent = "Location";
+    div.appendChild(h2);
+
+    const p = document.createElement("p");
+    p.textContent = "99 Ice Cream Lane, Mississauga ON L5B 2C9";
+    div.appendChild(p);
+
+    return div;
+})();
+
+const phoneNum = (() => {
+    const div = document.createElement("div");
+
+    const h2 = document.createElement("h2");
+    h2.textContent = "Call us";
+    div.appendChild(h2);
+
+    const p = document.createElement("p");
+    p.textContent = "647 123 4567";
+    div.appendChild(p);
+
+    return div;
+})();
+
+const openingHrs = (() => {
+    const div = document.createElement("div");
+
+    const h2 = document.createElement("h2");
+    h2.textContent = "Opening hours";
+    div.appendChild(h2);
+
+    const p = document.createElement("p");
+    p.textContent = "4 p.m. to 10 p.m. everyday";
+    div.appendChild(p);
+
+    return div;
+})();
+
 function createContactContent(parent) {
-    console.log("contact content created");
+    const div = document.createElement("div");
+    div.setAttribute("id", "contact-content");
+
+    div.appendChild(contact_location);
+    div.appendChild(phoneNum);
+    div.appendChild(openingHrs);
+
+    parent.appendChild(div);
 }
 
 /* harmony default export */ const contact = (createContactContent);
--- src/js/contact.js
+++ src/js/contact.js
@@ -1,5 +1,54 @@
+const location = (() => {
+    const div = document.createElement("div");
+
+    const h2 = document.createElement("h2");
+    h2.textContent = "Location";
+    div.appendChild(h2);
+
+    const p = document.createElement("p");
+    p.textContent = "99 Ice Cream Lane, Mississauga ON L5B 2C9";
+    div.appendChild(p);
+
+    return div;
+})();
+
+const phoneNum = (() => {
+    const div = document.createElement("div");
+
+    const h2 = document.createElement("h2");
+    h2.textContent = "Call us";
+    div.appendChild(h2);
+
+    const p = document.createElement("p");
+    p.textContent = "647 123 4567";
+    div.appendChild(p);
+
+    return div;
+})();
+
+const openingHrs = (() => {
+    const div = document.createElement("div");
+
+    const h2 = document.createElement("h2");
+    h2.textContent = "Opening hours";
+    div.appendChild(h2);
+
+    const p = document.createElement("p");
+    p.textContent = "4 p.m. to 10 p.m. everyday";
+    div.appendChild(p);
+
+    return div;
+})();
+
 function createContactContent(parent) {
-    console.log("contact content created");
+    const div = document.createElement("div");
+    div.setAttribute("id", "contact-content");
+
+    div.appendChild(location);
+    div.appendChild(phoneNum);
+    div.appendChild(openingHrs);
+
+    parent.appendChild(div);
 }
 
 export default createContactContent;
